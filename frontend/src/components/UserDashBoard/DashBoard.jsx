45182f0c4434d9fef853543f6f3bbfa6c267e0df
Subhajit Kundu <kundusubhajit73@gmail.com>
2023-12-30 19:42:19 +0530

    axios added to frontend

--- frontend/src/components/UserDashBoard/DashBoard.jsx
+++ frontend/src/components/UserDashBoard/DashBoard.jsx
@@ -3,28 +3,28 @@ import SideBar from './SideBar';
 import { Outlet } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { setDocUpload } from '../../redux/Slices/registration';
-import axios from 'axios';
+// import axios from 'axios';
 
 const DashBoard = () => {
   const dispatch = useDispatch();
   const docUploaded = useSelector((store) => store.registration.docUploaded);
-  useEffect(() => {
-    !docUploaded && getDocs();
-  }, []);
-  const getDocs = async () => {
-    try {
-      const res = await axios.get('/api/v1/users/getDocs', {
-        withCredentials: true,
-      });
-      dispatch(setDocUpload(res.data.data.data.documents));
-      
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // useEffect(() => {
+  //  !docUploaded && getDocs();
+  // }, []);
+  // const getDocs = async () => {
+  //   try {
+  //     const res = await axios.get('/api/v1/users/getDocs', {
+  //       withCredentials: true,
+  //     });
+  //     dispatch(setDocUpload(res.data.data.data.documents));
+
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   return (
-    <div className='flex justify-around'>
+    <div className="flex justify-around">
       <SideBar />
       <Outlet />
     </div>
